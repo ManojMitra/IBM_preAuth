d6966c3b217446f76888354233e91decb9dcd45d
ManojMitra <manojmitra@in.ibm.com>
2016-11-23 12:47:40 +0530

    updated code

--- preAuth/preAuth.go
+++ preAuth/preAuth.go
@@ -126,22 +126,11 @@ func (t *SimpleChaincode) writeDummyProvider(stub shim.ChaincodeStubInterface) (
 
 // Invoke is our entry point to invoke a chaincode function
 func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	fmt.Println("invoke is running MM" + function)
-	fmt.Println(args)
+	fmt.Println("invoke called for " + function)
 
 	// Handle different functions
-	if function == "init" {
-		return t.Init(stub, "init", args)
-	} else if function == "writeProvider" {
-		return t.write(stub, "writeProvider", args)
-	} else if function == "writeMember" {
-		return t.write(stub, "writeMember", args)
-	} else if function == "writeService" {
-		return t.write(stub, "writeService", args)
-	} else if function == "writePayer" {
-		return t.write(stub, "writePayer", args)
-	} else if function == "writePreAuth" {
-		return t.write(stub, "writePreAuth", args)
+	if function == "write" {
+		return t.write(stub, "write", args)
 	}
 
 	fmt.Println("invoke did not find func: " + function) //error
@@ -153,40 +142,36 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 	fmt.Println("query is running MM " + function)
 
 	// Handle different functions
-	if function == "readProvider" {
-		return t.read(stub, "readProvider", args[0])
-	} else if function == "readMember" {
-		return t.read(stub, "readMember", args[0])
-	} else if function == "readService" {
-		return t.read(stub, "readService", args[0])
-	} else if function == "readPayer" {
-		return t.read(stub, "readPayer", args[0])
-	} else if function == "readPreAuth" {
-		return t.read(stub, "readPreAuth", args[0])
+	if function == "read" {
+		return t.read(stub, "read", args)
 	}
 
 	return nil, errors.New("Received unknown function query: " + function)
 }
 
-// Add to chaincode method
-func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	fmt.Println("Write called for " + function)
-	var key, jsonResp string
-	var err error
-	var rcvdKey, chaincodeObj []byte
+// Add structure
+func (t *SimpleChaincode) writeStruct(stub shim.ChaincodeStubInterface, structName string, args []string) ([]byte, error) {
+	fmt.Println("Write called for " + structName)
+	var key string
 
-	if function == "writeProvider" {
+	if structName == "writeProvider" {
 		if len(args) == 6 {
 			key = "PRO" + strconv.Itoa(rand.Intn(1000))
 			fmt.Println("Provider Key is: " + key)
+
 			pt := &Provider{args[0], args[1], args[2], args[3], args[4], args[5]}
+
 			jsonAsBytes, _ := json.Marshal(pt)
-			err = stub.PutState(key, jsonAsBytes)
+			err := stub.PutState(key, jsonAsBytes)
+			if err != nil {
+				jsonResp := "{\"Error\":\"Failed to perform operation}"
+				return nil, errors.New(jsonResp)
+			}
 			fmt.Println("Provider added successfully with key: " + key)
 		} else {
 			return nil, errors.New("Incorrect number of arguments. Expecting 6")
 		}
-	} else if function == "writeMember" {
+	} else if structName == "writeMember" {
 		if len(args) == 4 {
 			key = "MEM" + strconv.Itoa(rand.Intn(1000))
 			fmt.Println("Member Key is: " + key)
@@ -194,12 +179,16 @@ func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, function strin
 			mem := &Member{args[0], args[1], args[2], args[3]}
 
 			jsonAsBytes, _ := json.Marshal(mem)
-			err = stub.PutState(key, jsonAsBytes)
+			err := stub.PutState(key, jsonAsBytes)
+			if err != nil {
+				jsonResp := "{\"Error\":\"Failed to perform operation}"
+				return nil, errors.New(jsonResp)
+			}
 			fmt.Println("Member added successfully with key: " + key)
 		} else {
 			return nil, errors.New("Incorrect number of arguments. Expecting 4")
 		}
-	} else if function == "writeService" {
+	} else if structName == "writeService" {
 		if len(args) == 12 {
 			key = "SRV" + strconv.Itoa(rand.Intn(1000))
 			fmt.Println("Service Key is: " + key)
@@ -207,12 +196,16 @@ func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, function strin
 			srv := &Service{args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9], args[10], args[11]}
 
 			jsonAsBytes, _ := json.Marshal(srv)
-			err = stub.PutState(key, jsonAsBytes)
+			err := stub.PutState(key, jsonAsBytes)
+			if err != nil {
+				jsonResp := "{\"Error\":\"Failed to perform operation}"
+				return nil, errors.New(jsonResp)
+			}
 			fmt.Println("Member added successfully with key: " + key)
 		} else {
 			return nil, errors.New("Incorrect number of arguments. Expecting 12")
 		}
-	} else if function == "writePayer" {
+	} else if structName == "writePayer" {
 		if len(args) == 9 {
 			key = "PYR" + strconv.Itoa(rand.Intn(1000))
 			fmt.Println("Service Key is: " + key)
@@ -220,114 +213,156 @@ func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, function strin
 			pyr := &Payer{args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8]}
 
 			jsonAsBytes, _ := json.Marshal(pyr)
-			err = stub.PutState(key, jsonAsBytes)
+			err := stub.PutState(key, jsonAsBytes)
+			if err != nil {
+				jsonResp := "{\"Error\":\"Failed to perform operation}"
+				return nil, errors.New(jsonResp)
+			}
 			fmt.Println("Member added successfully with key: " + key)
 		} else {
 			return nil, errors.New("Incorrect number of arguments. Expecting 9")
 		}
-	} else if function == "writePreAuth" {
-		key = "PA" + strconv.Itoa(rand.Intn(10000000))
-		fmt.Println("PreAuth Key is: " + key)
-
-		preAuth := &PreAuthForm{}
-		preAuth.PreAuthID = key
-		preAuth.PreAuthStatus = "Submitted"
-
-		if len(args) == 6 {
-			rcvdKey, err = t.write(stub, "writeProvider", args)
-			if err != nil {
-				jsonResp = "{\"Error\":\"Failed to perform operation}"
-				return nil, errors.New(jsonResp)
-			}
-			readKey := fmt.Sprintf("%s", rcvdKey)
-			chaincodeObj, err = t.read(stub, "readProvider", readKey)
-			buf := bytes.NewReader(chaincodeObj)
-			var p Provider
-			err := binary.Read(buf, binary.LittleEndian, &p)
-
-			if err != nil {
-				jsonResp = "{\"Error\":\"Failed to perform operation}"
-				return nil, errors.New(jsonResp)
-			}
-			preAuth.PreAuthProvider = p
-		} else if len(args) == 10 {
-			rcvdKey, err = t.write(stub, "writeProvider", args[5:9])
-			if err != nil {
-				jsonResp = "{\"Error\":\"Failed to perform operation}"
-				return nil, errors.New(jsonResp)
-			}
-			readKey := fmt.Sprintf("%s", rcvdKey)
-			chaincodeObj, err = t.read(stub, "readMember", readKey)
-			buf := bytes.NewReader(chaincodeObj)
-			var mem Member
-			err := binary.Read(buf, binary.LittleEndian, &mem)
-
-			if err != nil {
-				jsonResp = "{\"Error\":\"Failed to perform operation}"
-				return nil, errors.New(jsonResp)
-			}
-			preAuth.PreAuthMember = mem
-		} else if len(args) == 22 {
-			rcvdKey, err = t.write(stub, "writeService", args[10:21])
-			if err != nil {
-				jsonResp = "{\"Error\":\"Failed to perform operation}"
-				return nil, errors.New(jsonResp)
-			}
-			readKey := fmt.Sprintf("%s", rcvdKey)
-			chaincodeObj, err = t.read(stub, "readService", readKey)
-			buf := bytes.NewReader(chaincodeObj)
-			var srv Service
-			err := binary.Read(buf, binary.LittleEndian, &srv)
-
-			if err != nil {
-				jsonResp = "{\"Error\":\"Failed to perform operation}"
-				return nil, errors.New(jsonResp)
-			}
-			preAuth.PreAuthService = srv
-		} else if len(args) == 31 {
-			rcvdKey, err = t.write(stub, "writePayer", args[22:30])
-			if err != nil {
-				jsonResp = "{\"Error\":\"Failed to perform operation}"
-				return nil, errors.New(jsonResp)
-			}
-			readKey := fmt.Sprintf("%s", rcvdKey)
-			chaincodeObj, err = t.read(stub, "readPayer", readKey)
-			buf := bytes.NewReader(chaincodeObj)
-			var pyr Payer
-			err := binary.Read(buf, binary.LittleEndian, &pyr)
-
-			if err != nil {
-				jsonResp = "{\"Error\":\"Failed to perform operation}"
-				return nil, errors.New(jsonResp)
-			}
-			preAuth.PreAuthPayer = pyr
-		} else {
-			return nil, errors.New("Incorrect number of arguments received.")
-		}
-		jsonAsBytes, _ := json.Marshal(preAuth)
-		err = stub.PutState(key, jsonAsBytes)
-	} else {
-		return nil, errors.New("Received unknown function invocation: " + function)
 	}
 
+	return []byte(key), nil
+}
+
+// Add to chaincode
+func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	fmt.Println("Write called for " + function)
+	var key, jsonResp string
+	var err error
+	var rcvdKey, chaincodeObj []byte
+
+	key = "PA" + strconv.Itoa(rand.Intn(10000000))
+	fmt.Println("PreAuth Key is: " + key)
+
+	preAuth := &PreAuthForm{}
+	preAuth.PreAuthID = key
+	preAuth.PreAuthStatus = "Submitted"
+
+	if len(args) == 6 {
+		rcvdKey, err = t.writeStruct(stub, "writeProvider", args)
+		if err != nil {
+			jsonResp = "{\"Error\":\"Failed to perform operation}"
+			return nil, errors.New(jsonResp)
+		}
+		readKey := fmt.Sprintf("%s", rcvdKey)
+		chaincodeObj, err = t.readStruct(stub, "readProvider", readKey)
+		buf := bytes.NewReader(chaincodeObj)
+		var p Provider
+		err := binary.Read(buf, binary.LittleEndian, &p)
+
+		if err != nil {
+			jsonResp = "{\"Error\":\"Failed to perform operation}"
+			return nil, errors.New(jsonResp)
+		}
+		preAuth.PreAuthProvider = p
+	} else if len(args) == 10 {
+		rcvdKey, err = t.writeStruct(stub, "writeProvider", args[5:9])
+		if err != nil {
+			jsonResp = "{\"Error\":\"Failed to perform operation}"
+			return nil, errors.New(jsonResp)
+		}
+		readKey := fmt.Sprintf("%s", rcvdKey)
+		chaincodeObj, err = t.readStruct(stub, "readMember", readKey)
+		buf := bytes.NewReader(chaincodeObj)
+		var mem Member
+		err := binary.Read(buf, binary.LittleEndian, &mem)
+
+		if err != nil {
+			jsonResp = "{\"Error\":\"Failed to perform operation}"
+			return nil, errors.New(jsonResp)
+		}
+		preAuth.PreAuthMember = mem
+	} else if len(args) == 22 {
+		rcvdKey, err = t.writeStruct(stub, "writeService", args[10:21])
+		if err != nil {
+			jsonResp = "{\"Error\":\"Failed to perform operation}"
+			return nil, errors.New(jsonResp)
+		}
+		readKey := fmt.Sprintf("%s", rcvdKey)
+		chaincodeObj, err = t.readStruct(stub, "readService", readKey)
+		buf := bytes.NewReader(chaincodeObj)
+		var srv Service
+		err := binary.Read(buf, binary.LittleEndian, &srv)
+
+		if err != nil {
+			jsonResp = "{\"Error\":\"Failed to perform operation}"
+			return nil, errors.New(jsonResp)
+		}
+		preAuth.PreAuthService = srv
+	} else if len(args) == 31 {
+		rcvdKey, err = t.writeStruct(stub, "writePayer", args[22:30])
+		if err != nil {
+			jsonResp = "{\"Error\":\"Failed to perform operation}"
+			return nil, errors.New(jsonResp)
+		}
+		readKey := fmt.Sprintf("%s", rcvdKey)
+		chaincodeObj, err = t.readStruct(stub, "readPayer", readKey)
+		buf := bytes.NewReader(chaincodeObj)
+		var pyr Payer
+		err := binary.Read(buf, binary.LittleEndian, &pyr)
+
+		if err != nil {
+			jsonResp = "{\"Error\":\"Failed to perform operation}"
+			return nil, errors.New(jsonResp)
+		}
+		preAuth.PreAuthPayer = pyr
+	} else {
+		return nil, errors.New("Incorrect number of arguments received.")
+	}
+	jsonAsBytes, _ := json.Marshal(preAuth)
+	err = stub.PutState(key, jsonAsBytes)
+
 	if err != nil {
-		return nil, err
+		return nil, errors.New("Received unknown function invocation: " + function)
 	}
 
 	return []byte(key), nil
 }
 
 // Read from chaincode method
-func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, function string, args string) ([]byte, error) {
+func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	fmt.Println("Read called for " + function)
 	var key, jsonResp string
-	var err error
 
 	if len(args) != 1 {
 		return nil, errors.New("Incorrect number of arguments. Expecting name of the var to query")
 	}
 
-	key = args
+	if len(args) == 1 {
+		key = args[0]
+		fmt.Println("Reading for key: " + key)
+		valAsbytes, err := stub.GetState(key)
+		if err != nil {
+			jsonResp = "{\"Error\":\"Failed to get object for " + key + "\"}"
+			return nil, errors.New(jsonResp)
+		}
+
+		var p PreAuthForm
+		err = json.Unmarshal(valAsbytes, &p)
+		if err != nil {
+			jsonResp = "{\"Error\":\"Failed to get PreAuth for " + key + "\"}"
+			return nil, errors.New(jsonResp)
+		}
+		fmt.Println("PreAuth fetched successfully with key: " + key + " : " + string(valAsbytes))
+		return valAsbytes, nil
+	} else if len(args) == 2 {
+		structName := args[0]
+		key = args[1]
+		return t.readStruct(stub, structName, key)
+	} else {
+		return nil, errors.New("Received unknown function query: " + function)
+	}
+}
+
+// Read from chaincode structure
+func (t *SimpleChaincode) readStruct(stub shim.ChaincodeStubInterface, structName string, arg string) ([]byte, error) {
+	fmt.Println("Read called for " + structName)
+	var key, jsonResp string
+	var err error
+
 	fmt.Println("Reading for key: " + key)
 	valAsbytes, err := stub.GetState(key)
 	if err != nil {
@@ -335,7 +370,7 @@ func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, function string
 		return nil, errors.New(jsonResp)
 	}
 
-	if function == "readProvider" {
+	if structName == "readProvider" {
 		var p Provider
 		err = json.Unmarshal(valAsbytes, &p)
 		if err != nil {
@@ -343,7 +378,7 @@ func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, function string
 			return nil, errors.New(jsonResp)
 		}
 		fmt.Println("Provider fetched successfully with key: ")
-	} else if function == "readMember" {
+	} else if structName == "readMember" {
 		var m Member
 		err = json.Unmarshal(valAsbytes, &m)
 		if err != nil {
@@ -351,7 +386,7 @@ func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, function string
 			return nil, errors.New(jsonResp)
 		}
 		fmt.Println("Member fetched successfully with key: ")
-	} else if function == "readService" {
+	} else if structName == "readService" {
 		var s Service
 		err = json.Unmarshal(valAsbytes, &s)
 		if err != nil {
@@ -359,7 +394,7 @@ func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, function string
 			return nil, errors.New(jsonResp)
 		}
 		fmt.Println("Service fetched successfully with key: ")
-	} else if function == "readPayer" {
+	} else if structName == "readPayer" {
 		var pyr Payer
 		err = json.Unmarshal(valAsbytes, &pyr)
 		if err != nil {
@@ -367,16 +402,8 @@ func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, function string
 			return nil, errors.New(jsonResp)
 		}
 		fmt.Println("Payer fetched successfully with key: ")
-	} else if function == "readPreAuth" {
-		if err != nil {
-			var p PreAuthForm
-			err = json.Unmarshal(valAsbytes, &p)
-			jsonResp = "{\"Error\":\"Failed to get PreAuth for " + key + "\"}"
-			return nil, errors.New(jsonResp)
-		}
-		fmt.Println("PreAuth fetched successfully with key: ")
 	} else {
-		return nil, errors.New("Received unknown function query: " + function)
+		return nil, errors.New("Received unknown structure or key in query: " + structName + ":" + key)
 	}
 
 	fmt.Print(key + " : " + string(valAsbytes))
